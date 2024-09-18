75fcef2f12c865b48dd5fbeff71edf9337062a47
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-09-18 22:03:10 +0600

    add money and cash out feature added

--- js/home.js
+++ js/home.js
@@ -5,11 +5,58 @@ addMoneyBtn.addEventListener('click', (event) =>{
     event.preventDefault();
 
     const amountNumber = document.getElementById('amountNumber').value;
+    
     const pinNumber = document.getElementById('pinNumber').value;
 
-    console.log(amountNumber, pinNumber);
+    const currentMoney = document.getElementById('currentMoney').innerText;
 
-    console.log("money added")
+    const updateBalace = parseFloat(currentMoney) + parseFloat(amountNumber);
+    
+    if(updateBalace && pinNumber){
+        document.getElementById('currentMoney').innerText = updateBalace;
+
+        document.getElementById('amountNumber').value = "";
+        document.getElementById('pinNumber').value = "";
+    }
+
+    else{
+        alert('please Enter amount or number');
+
+        document.getElementById('amountNumber').value = "";
+        document.getElementById('pinNumber').value = "";
+    }
+
+
+})
+
+
+const cashOut = document.getElementById('cashOut');
+
+cashOut.addEventListener('click', () => {
+
+    event.preventDefault();
+
+    const amountNumber = document.getElementById('amountNumber').value;
+    
+    const pinNumber = document.getElementById('pinNumber').value;
+
+    const currentMoney = document.getElementById('currentMoney').innerText;
+
+    const updateBalace = parseFloat(currentMoney) - parseFloat(amountNumber);
+    
+    if(updateBalace && pinNumber){
+        document.getElementById('currentMoney').innerText = updateBalace;
+
+        document.getElementById('amountNumber').value = "";
+        document.getElementById('pinNumber').value = "";
+    }
+
+    else{
+        alert('please Enter amount or number');
+
+        document.getElementById('amountNumber').value = "";
+        document.getElementById('pinNumber').value = "";
+    }
 
 
 })
